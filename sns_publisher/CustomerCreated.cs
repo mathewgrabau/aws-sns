de63abb0f4f802a665896d9caf3a658e4c58813d
Mathew Grabau <mgrabau@mathewgrabau.ca>
2023-01-27 00:59:23 -0600

    Pubshing messages

---
+++ sns_publisher/CustomerCreated.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SqsPublisher;
+
+public class CustomerCreated
+{
+    public required Guid Id { get; init; }
+    public required string FullName { get; init; }
+    public required string Email { get; init; }
+    public required string GithubUsername { get; init; }
+    public required DateTime DateOfBirth { get; init; }
+}
---
+++ sns_publisher/Program.cs
@@ -0,0 +1,41 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using SqsPublisher;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+
+// using the authentcation already available
+var sqsClient = new AmazonSQSClient();
+
+var customerMessage = new CustomerCreated
+{
+    Id = Guid.NewGuid(),
+    FullName = "Joe Cool",
+    GithubUsername = "jcool",
+    Email = "someone@something.com",
+    DateOfBirth = DateTime.Parse("1999-01-01")
+};
+
+// Don't need the specific URL, can get it without hardcoding
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("customers");
+Console.WriteLine("Retrieved the queue URL: " + queueUrlResponse.QueueUrl);
+
+var request = new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customerMessage),
+    // Adding a type attribute
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>()
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = "CustomerCreated"
+            }
+        }
+    }
+};
+var response = await sqsClient.SendMessageAsync(request);
+
+Console.WriteLine(response.MessageId);
